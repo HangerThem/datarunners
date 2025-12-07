cca775f4b78ece7a9ad3bb25269eea8e959e1ddf
HangerThem <f.borisjuk@gmail.com>
2025-12-07 21:54:06 +0100

    main/index.ts fix

--- src/main/index.ts
+++ src/main/index.ts
@@ -27,7 +27,6 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
-    mainWindow.webContents.openDevTools()
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
